68092f71c4f70e457c8ce13f71efe6ba9959de9b
LanYc <lanyecheng@126.com>
2020-08-10 22:23:37 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -3,10 +3,28 @@
 # author：albert time:2020/8/10
 
 from flask import Flask
+from flask import escape, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Welcome to My Watchlist!'
+    return 'Hello'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是调用示例
+    print(url_for('hello'))
+    print(url_for('user_page', name='Bo bo'))
+    print(url_for('user_page', name='Mr.zhang'))
+    print(url_for('test_url_for'))
+    # 下面这个调用传入多余的关键字参数，它们会作为查询字符串附加到 url 后面
+    print(url_for('test_url_for', num=2))
+    return 'Test Page'
